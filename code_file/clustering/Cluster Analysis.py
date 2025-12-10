14b0f280c0148c44c5a898af01c6fbb96a6fa5b4
Eileen Chen <ec.eileenchen@gmail.com>
2025-12-10 09:58:30 -0800

    Add files via upload

---
+++ code_file/clustering/Cluster Analysis.py
@@ -0,0 +1,237 @@
+import os
+import pandas as pd
+
+os.chdir(r'C:\Users\eilee\OneDrive\Documents\CSS 499\cluster analysis data')
+#print(os.getcwd())
+
+
+# SpectralClustering 4d
+s_output = pd.read_csv('Label_2_4d_spectral_clustering_output.csv')
+features = ['name', 'Guide', 'avg T0 log2fc', 'avg T24 log2fc', 'avg T48 log2fc', 'avg T72 log2fc', 'rank', 
+            'slope1_score', 'slope2_score', 'slope3_score', 'slope4_score', 'slope5_score', 'slope6_score',
+            'total_slope_score', 'average_slope', 'dbscan_label']
+
+# label 1 min avg_slope -->  -0.045938
+minidx1 = s_output.loc[s_output['dbscan_label'] == 1, 'average_slope'].idxmin()
+cluster1min = s_output.loc[minidx1, features]
+cluster1min
+
+# label 1 max avg_slope --> 0.059411
+maxidx1 = s_output.loc[s_output['dbscan_label'] == 1, 'average_slope'].idxmax()
+cluster1max = s_output.loc[maxidx1, features]
+cluster1max
+
+# label 0 min avg_slope --> -0.182162
+minidx0 = s_output.loc[s_output['dbscan_label'] == 0, 'average_slope'].idxmin()
+cluster0min = s_output.loc[minidx0, features]
+cluster0min
+
+# label 0 max avg_slope --> -0.006309
+maxidx0 = s_output.loc[s_output['dbscan_label'] == 0, 'average_slope'].idxmax()
+cluster0max = s_output.loc[maxidx0, features]
+cluster0max
+
+## total_slope
+# label 1 min total_slope -->  -0.045938
+minidx1 = s_output.loc[s_output['dbscan_label'] == 1, 'total_slope_score'].idxmin()
+cluster1min = s_output.loc[minidx1, features]
+cluster1min
+
+# label 1 max total_slope --> 0.059411
+maxidx1 = s_output.loc[s_output['dbscan_label'] == 1, 'total_slope_score'].idxmax()
+cluster1max = s_output.loc[maxidx1, features]
+cluster1max
+
+# label 0 min total_slope --> -0.182162
+minidx0 = s_output.loc[s_output['dbscan_label'] == 0, 'total_slope_score'].idxmin()
+cluster0min = s_output.loc[minidx0, features]
+cluster0min
+
+# label 0 max total_slope --> -0.006309
+maxidx0 = s_output.loc[s_output['dbscan_label'] == 0, 'total_slope_score'].idxmax()
+cluster0max = s_output.loc[maxidx0, features]
+cluster0max
+
+print(s_output.loc[s_output['dbscan_label'] == 1,'avg T72 log2fc']) ## avg T72 log2fc 6.837164503 ~ 16.56885313 
+print(s_output.loc[s_output['dbscan_label'] == 0,'avg T72 log2fc']) ## avg T72 log2fc 0 ~ 11.75263585
+
+## avg T72 log2fc
+# label 1 min total_slope -->  -0.045938
+minidx1 = s_output.loc[s_output['dbscan_label'] == 1, 'avg T72 log2fc'].idxmin()
+cluster1min = s_output.loc[minidx1, features]
+cluster1min
+
+# label 1 max total_slope --> 0.059411
+maxidx1 = s_output.loc[s_output['dbscan_label'] == 1, 'avg T72 log2fc'].idxmax()
+cluster1max = s_output.loc[maxidx1, features]
+cluster1max
+
+# label 0 min total_slope --> -0.182162
+minidx0 = s_output.loc[s_output['dbscan_label'] == 0, 'avg T72 log2fc'].idxmin()
+cluster0min = s_output.loc[minidx0, features]
+cluster0min
+
+# label 0 max total_slope --> -0.006309
+maxidx0 = s_output.loc[s_output['dbscan_label'] == 0, 'avg T72 log2fc'].idxmax()
+cluster0max = s_output.loc[maxidx0, features]
+cluster0max
+
+s_output.groupby('dbscan_label')['rank'].value_counts()
+
+# SpectralClustering 7d
+s7_output = pd.read_csv('7_spectral_clustering_output.csv')
+features = ['name', 'Guide', 'avg T0 log2fc', 'avg T24 log2fc', 'avg T48 log2fc', 'avg T72 log2fc',
+            'slope1_score', 'slope2_score', 'slope3_score', 'slope4_score', 'slope5_score', 'slope6_score',
+            'total_slope_score', 'average_slope', 'dbscan_label']
+
+# label 1 min avg_slope -->   -0.042854
+minidx1 = s7_output.loc[s7_output['dbscan_label'] == 1, 'average_slope'].idxmin()
+cluster1min = s7_output.loc[minidx1, features]
+cluster1min
+
+# label 1 max avg_slope --> 0.059411
+maxidx1 = s7_output.loc[s7_output['dbscan_label'] == 1, 'average_slope'].idxmax()
+cluster1max = s7_output.loc[maxidx1, features]
+cluster1max
+
+# label 0 min avg_slope --> -0.182162
+minidx0 = s7_output.loc[s7_output['dbscan_label'] == 0, 'average_slope'].idxmin()
+cluster0min = s7_output.loc[minidx0, features]
+cluster0min
+
+# label 0 max avg_slope --> -0.006309
+maxidx0 = s7_output.loc[s7_output['dbscan_label'] == 0, 'average_slope'].idxmax()
+cluster0max = s7_output.loc[maxidx0, features]
+cluster0max
+
+print(s7_output.loc[s7_output['dbscan_label'] == 1,'avg T72 log2fc']) ## avg T72 log2fc 6.837164503 ~ 16.56885313 
+print(s7_output.loc[s7_output['dbscan_label'] == 0,'avg T72 log2fc']) ## avg T72 log2fc 0 ~ 11.75263585
+
+s7_output.groupby('dbscan_label')['rank'].value_counts()
+
+# SpectralClustering 2d
+s2_output = pd.read_csv('2_spectral_clustering_output.csv')
+features = ['name', 'Guide', 'avg T0 log2fc', 'avg T24 log2fc', 'avg T48 log2fc', 'avg T72 log2fc',
+            'slope1_score', 'slope2_score', 'slope3_score', 'slope4_score', 'slope5_score', 'slope6_score',
+            'total_slope_score', 'average_slope', 'dbscan_label']
+
+# label 1 min avg_slope -->   -0.041034
+minidx1 = s2_output.loc[s2_output['dbscan_label'] == 1, 'average_slope'].idxmin()
+cluster1min = s2_output.loc[minidx1, features]
+cluster1min
+
+# label 1 max avg_slope --> 0.059411
+maxidx1 = s2_output.loc[s2_output['dbscan_label'] == 1, 'average_slope'].idxmax()
+cluster1max = s2_output.loc[maxidx1, features]
+cluster1max
+
+# label 0 min avg_slope --> -0.182162
+minidx0 = s2_output.loc[s2_output['dbscan_label'] == 0, 'average_slope'].idxmin()
+cluster0min = s2_output.loc[minidx0, features]
+cluster0min
+
+# label 0 max avg_slope --> -0.005403
+maxidx0 = s2_output.loc[s2_output['dbscan_label'] == 0, 'average_slope'].idxmax()
+cluster0max = s2_output.loc[maxidx0, features]
+cluster0max
+
+print(s2_output.loc[s2_output['dbscan_label'] == 1,'avg T72 log2fc']) ## avg T72 log2fc 8.153812177 ~ 16.56885313 
+print(s2_output.loc[s2_output['dbscan_label'] == 0,'avg T72 log2fc']) ## avg T72 log2fc 0 ~ 9.712423102
+
+s2_output.groupby('dbscan_label')['rank'].value_counts()
+
+s2_output.groupby('rank')['rank'].value_counts()
+
+# Mean Shift 4d
+output_4 = pd.read_csv('4_MeanShift_clustering_output.csv')
+features = ['name', 'Guide', 'avg T0 log2fc', 'avg T24 log2fc', 'avg T48 log2fc', 'avg T72 log2fc',
+            'slope1_score', 'slope2_score', 'slope3_score', 'slope4_score', 'slope5_score', 'slope6_score',
+            'total_slope_score', 'average_slope', 'dbscan_label']
+
+# dbscan label 1 min avg_slope -->   -0.06525
+minidx1 = output_4.loc[output_4['dbscan_label'] == 1, 'average_slope'].idxmin()
+cluster1min = output_4.loc[minidx1, features]
+cluster1min
+
+# dbscan label 1 max avg_slope --> 0.059411
+maxidx1 = output_4.loc[output_4['dbscan_label'] == 1, 'average_slope'].idxmax()
+cluster1max = output_4.loc[maxidx1, features]
+cluster1max
+
+# dbscan label 0 min avg_slope --> -0.182162
+minidx0 = output_4.loc[output_4['dbscan_label'] == 0, 'average_slope'].idxmin()
+cluster0min = output_4.loc[minidx0, features]
+cluster0min
+
+# dbscan label 0 max avg_slope -->  -0.051458
+maxidx0 = output_4.loc[output_4['dbscan_label'] == 0, 'average_slope'].idxmax()
+cluster0max = output_4.loc[maxidx0, features]
+cluster0max
+
+print(output_4.loc[output_4['dbscan_label'] == 1,'avg T72 log2fc']) ## avg T72 log2fc 3.642206199 ~ 16.56885313
+
+print(output_4.loc[output_4['dbscan_label'] == 0,'avg T72 log2fc']) ## avg T72 log2fc 0 ~ 8.948233691
+
+output_4.groupby('dbscan_label')['rank'].value_counts()
+
+# Mean Shift with 48 and 72 instead of 24
+output_7 = pd.read_csv('7_MeanShift_clustering_output.csv')
+features = ['name', 'Guide', 'avg T0 log2fc', 'avg T24 log2fc', 'avg T48 log2fc', 'avg T72 log2fc',
+            'slope1_score', 'slope2_score', 'slope3_score', 'slope4_score', 'slope5_score', 'slope6_score',
+            'total_slope_score', 'average_slope', 'dbscan_label']
+
+# dbscan label 1 min avg_slope -->  -0.06525
+minidx1 = output_7.loc[output_7['dbscan_label'] == 1, 'average_slope'].idxmin()
+cluster1min = output_7.loc[minidx1, features]
+cluster1min
+
+# dbscan label 1 max avg_slope --> 0.059411
+maxidx1 = output_7.loc[output_7['dbscan_label'] == 1, 'average_slope'].idxmax()
+cluster1max = output_7.loc[maxidx1, features]
+cluster1max
+
+# dbscan label 0 min avg_slope --> -0.182162
+minidx0 = output_7.loc[output_7['dbscan_label'] == 0, 'average_slope'].idxmin()
+cluster0min = output_7.loc[minidx0, features]
+cluster0min
+
+# dbscan label 0 max avg_slope -->  -0.044101
+maxidx0 = output_7.loc[output_7['dbscan_label'] == 0, 'average_slope'].idxmax()
+cluster0max = output_7.loc[maxidx0, features]
+cluster0max
+
+print(output_7.loc[output_7['dbscan_label'] == 1,'avg T72 log2fc']) ## avg T72 log2fc 3.642206199 ~ 16.56885313
+print(output_7.loc[output_7['dbscan_label'] == 0,'avg T72 log2fc']) ## avg T72 log2fc 0 ~ 9.053816864
+
+output_7.groupby('dbscan_label')['rank'].value_counts()
+
+# Mean Shift 2D
+output_2 = pd.read_csv('2_meanshift_clustering_output.csv')
+features = ['name', 'Guide', 'avg T0 log2fc', 'avg T24 log2fc', 'avg T48 log2fc', 'avg T72 log2fc',
+            'slope1_score', 'slope2_score', 'slope3_score', 'slope4_score', 'slope5_score', 'slope6_score',
+            'total_slope_score', 'average_slope', 'dbscan_label']
+
+# dbscan label 1 min avg_slope -->   -0.06221
+minidx1 = output_2.loc[output_2['dbscan_label'] == 1, 'average_slope'].idxmin()
+cluster1min = output_2.loc[minidx1, features]
+cluster1min
+
+# dbscan label 1 max avg_slope --> 0.059411
+maxidx1 = output_2.loc[output_2['dbscan_label'] == 1, 'average_slope'].idxmax()
+cluster1max = output_2.loc[maxidx1, features]
+cluster1max
+
+# dbscan label 0 min avg_slope --> -0.182162
+minidx0 = output_2.loc[output_2['dbscan_label'] == 0, 'average_slope'].idxmin()
+cluster0min = output_2.loc[minidx0, features]
+cluster0min
+
+# dbscan label 0 max avg_slope -->  -0.014602
+maxidx0 = output_2.loc[output_2['dbscan_label'] == 0, 'average_slope'].idxmax()
+cluster0max = output_2.loc[maxidx0, features]
+cluster0max
+
+print(output_2.loc[output_2['dbscan_label'] == 1,'avg T72 log2fc']) ## avg T72 log2fc 6.837164503 ~ 16.56885313
+print(output_2.loc[output_2['dbscan_label'] == 0,'avg T72 log2fc']) ## avg T72 log2fc 0 ~ 8.227012593
+
+output_2.groupby('dbscan_label')['rank'].value_counts()
---
+++ code_file/clustering/Clustering.py
@@ -0,0 +1,217 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN, SpectralBiclustering
+from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import SpectralClustering, OPTICS, MeanShift, estimate_bandwidth, AgglomerativeClustering
+from tensorflow.python.keras.utils.vis_utils import plot_model
+from hdbscan import HDBSCAN
+
+os.chdir(r'C:\Users\eilee\OneDrive\Documents\CSS 499')
+#print(os.getcwd())
+
+dataset = pd.read_csv('OH_Ranked_Guides.csv')
+dataset.head(5)
+
+slope1_scores = (dataset['avg T24 log2fc'] - dataset['avg T0 log2fc']) / 24
+#slope1_scores
+slope2_scores = (dataset['avg T48 log2fc'] - dataset['avg T24 log2fc']) / 24
+slope3_scores = (dataset['avg T72 log2fc'] - dataset['avg T48 log2fc']) / 24
+
+#slope4_scores = (dataset['avg T24 log2fc'] - dataset['avg T0 log2fc']) / 24
+#slope1_scores
+#slope4_scores = (dataset['avg T48 log2fc'] - dataset['avg T0 log2fc']) / 24
+#slope5_scores = (dataset['avg T72 log2fc'] - dataset['avg T0 log2fc']) / 24
+slope4_scores = (dataset['avg T48 log2fc'] - dataset['avg T0 log2fc']) / 48
+slope5_scores = (dataset['avg T72 log2fc'] - dataset['avg T0 log2fc']) / 72
+slope6_scores = (dataset['avg T72 log2fc'])
+
+t = np.array([0, 24, 48, 72]).reshape(-1, 1) 
+
+total_slope = np.array(dataset.loc[:, ['avg T0 log2fc', 'avg T24 log2fc', 'avg T48 log2fc', 'avg T72 log2fc']])
+
+slope_scores = []
+
+for row in total_slope:
+    model = LinearRegression().fit(t, row)
+    slope_scores.append(model.coef_[0])
+
+
+dataset['slope1_score'] = slope1_scores
+dataset['slope2_score'] = slope2_scores
+dataset['slope3_score'] = slope3_scores
+dataset['total_slope_score'] = slope_scores
+dataset['slope4_score'] = slope4_scores
+dataset['slope5_score'] = slope5_scores
+dataset['slope6_score'] = slope6_scores
+dataset['average_slope'] = (slope1_scores + slope2_scores + slope3_scores)/3
+
+
+
+#input_4d = dataset[['slope1_score', 'slope2_score', 'slope3_score', 'avg T72 log2fc']]
+input_4d = dataset[['slope1_score', 'slope2_score', 'slope3_score', 'total_slope_score']]
+input_7d = dataset[['slope1_score', 'slope2_score', 'slope3_score', 'total_slope_score', 'slope4_score', 'slope5_score', 'slope6_score']]
+input_2d = dataset[['slope6_score', 'average_slope']]
+input_4d = input_2d
+input_4d = input_7d
+input_4d ## (5633, 4)
+
+input_4d = StandardScaler().fit_transform(input_4d)
+
+
+neigh = NearestNeighbors()
+nbrs = neigh.fit(input_4d)
+distances, indices = nbrs.kneighbors(input_4d)
+k_distances = np.sort(distances[:, 4])
+
+plt.plot(k_distances)
+plt.ylabel("5-NN distance")
+plt.xlabel("Points sorted by distance")
+plt.title("k-distance graph")
+plt.grid(True)
+plt.show()
+
+#db = DBSCAN(eps=0.3, min_samples=10).fit(input_4d)
+#labels = db.labels_
+#print (labels)
+
+
+#db = KMeans(n_clusters= 3).fit(input_4d)
+#labels = db.labels_
+#print (labels)
+
+spectral_rbf = SpectralClustering(n_clusters=2, affinity='rbf')
+labels = spectral_rbf.fit_predict(input_4d)
+print (labels)
+
+sb = SpectralBiclustering(    n_clusters=2,
+    method='log',          # better for many datasets
+    n_components=6,        # helps stability
+    random_state=42
+)
+sb.fit(input_4d)
+
+print(sb.row_labels_)     # cluster labels for each row
+print(sb.column_labels_)  # cluster labels for each feature/column
+labels = sb.row_labels_
+
+bandwidth = estimate_bandwidth(input_4d, quantile=0.25, n_samples=len(input_4d))
+db = MeanShift(bandwidth=bandwidth, bin_seeding=True).fit(input_4d)
+labels = db.labels_
+#print (labels)
+
+#db = OPTICS(min_samples=10, xi=0.0001, min_cluster_size=0.0001).fit(input_4d)
+#labels = db.labels_
+#print(labels)
+
+db = HDBSCAN(
+    min_cluster_size=50,
+    min_samples=5,
+    metric='euclidean',
+    cluster_selection_method='eom'
+).fit(input_4d)
+labels = db.labels_
+print(labels)
+
+db = AgglomerativeClustering(
+    distance_threshold = 80,
+    n_clusters = None)
+labels = db.fit_predict(input_4d)
+print(labels)
+
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+print("Estimated number of clusters: %d" % n_clusters_)
+print("Estimated number of noise points: %d" % n_noise_)
+
+dataset['dbscan_label'] = labels
+print(dataset['dbscan_label'].value_counts())
+
+tsne = TSNE(n_components= 2)
+plot_2d = tsne.fit_transform(input_4d)
+#plot_2d = input_4d
+
+plt.scatter(plot_2d[:, 0], plot_2d[:, 1])
+plt.title('t-SNE Projection (4D to 2D)')
+plt.xlabel('TSNE-1')
+plt.ylabel('TSNE-2')
+plt.grid(False)
+plt.show()
+
+unique_labels = set(labels)
+core_samples_mask = np.zeros_like(labels, dtype=bool)
+#core_samples_mask[db.core_sample_indices_] = True
+
+colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = labels == k
+
+    xy = plot_2d[class_member_mask & core_samples_mask]
+    plt.plot(
+        xy[:, 0],
+        xy[:, 1],
+        "o",
+        markerfacecolor=tuple(col),
+        markeredgecolor="k",
+        markersize=14,
+    )
+
+    xy = plot_2d[class_member_mask & ~core_samples_mask]
+    plt.plot(
+        xy[:, 0],
+        xy[:, 1],
+        "o",
+        markerfacecolor=tuple(col),
+        markeredgecolor="k",
+        markersize=6,
+    )
+
+plt.title(f"Estimated number of clusters: {n_clusters_}")
+plt.savefig('2D.png')
+plt.show()
+
+
+dataset.groupby('dbscan_label')['dbscan_label'].value_counts()
+dataset.groupby('dbscan_label')['rank'].value_counts()
+
+
+dataset.to_csv('4_agg_clustering_output.csv')
+
+
+
+
+plt.scatter(plot_2d[:, 0], plot_2d[:, 1], c=labels)
+plt.title('DBSCAN Clusters on t-SNE projection')
+plt.xlabel('TSNE-1')
+plt.ylabel('TSNE-2')
+plt.colorbar(label='Cluster Label')
+plt.grid(False)
+plt.show()
+
+
+
+mask = labels != -1
+
+# Filter the t-SNE and labels arrays
+filtered_plot_2d = plot_2d[mask]
+filtered_labels = labels[mask]
+
+# Plot only the non-noise points
+plt.figure(figsize=(8, 6))
+plt.scatter(filtered_plot_2d[:, 0], filtered_plot_2d[:, 1], c=filtered_labels, s=10)
+plt.title('DBSCAN Clusters on t-SNE (Noise Removed)')
+plt.xlabel('TSNE-1')
+plt.ylabel('TSNE-2')
+plt.colorbar(label='Cluster Label')
+plt.grid(False)
+plt.show()
